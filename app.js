2ddb01c3dfbf47b243428352a75259fda2f49ef9
yamacode <haqi.yama@gmail.com>
2019-09-24 01:14:44 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('<h1>My node app</h1>');
+});
+
+
+app.listen(5000, () => {
+    console.log("app listening on port 5000");
+});
